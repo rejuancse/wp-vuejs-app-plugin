e29c3327c0d73036df74ff5a08dc479dc2d8354f
rejuancse <rejuancse@gmail.com>
2021-07-02 11:49:13 +0600

    Naming changes

--- plugin.php
+++ plugin.php
@@ -1,14 +1,9 @@
 <?php
 /**
- * @link            https://wp-vue-kickstart.com
- * @since           1.0.0
- * @package         WP_Vue_KickStart
- *
- * Plugin Name: WP Vue KickStart
- * Plugin URI: https://wp-vue-kickstart.com
+ * Plugin Name: WP Vue App
  * Description: A wp vue starter for plugin development.
  * Version: 1.0.0
- * Author: Md. Rabiul Islam Robi
+ * Author: Rejuan Ahamed
  * Author URI: https://robizstory.me
  * License: GPL v3
  * Text-Domain: textdomain
